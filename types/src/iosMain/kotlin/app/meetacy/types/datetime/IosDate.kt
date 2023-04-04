9da6bea1f28385cb141279433134cdd14f9e2150
Mark Dubkov <mark.dubkov@gmail.com>
2023-04-04 22:17:38 +0700

    WIP: work with datetime in ios target

---
+++ types/src/iosMain/kotlin/app/meetacy/types/datetime/IosDate.kt
@@ -0,0 +1,24 @@
+@file:OptIn(UnsafeConstructor::class)
+
+package app.meetacy.types.datetime
+
+import app.meetacy.types.annotation.UnsafeConstructor
+import platform.Foundation.NSDateFormatter
+import platform.Foundation.NSDate as IosDate
+
+public val IosDate.meetacyDate: Date
+    get() = Date(toISOString().take(10))
+
+internal fun IosDate(iso8601: String): IosDate =
+    NSDateFormatter.dateFromIsoString(iso8601)
+        ?: error("NSDate represents is nil")
+
+internal fun IosDate.toISOString(): String =
+    NSDateFormatter.isoStringFromDate(this)
+
+public val Date.iosDate: IosDate get() = IosDate(iso8601)
+
+public val DateTime.iosDate: IosDate get() = IosDate(iso8601)
+
+public val IosDate.meetacyDateTime: DateTime
+    get() = DateTime(toISOString())
