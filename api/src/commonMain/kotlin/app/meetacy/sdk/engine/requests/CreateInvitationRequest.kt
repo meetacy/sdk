6667875d8368b2e0bff08e20671156b2fead0cd5
bpavuk <boopentiy@gmail.com>
2023-06-02 14:34:05 +0300

    Updated create request

--- api/src/commonMain/kotlin/app/meetacy/sdk/engine/requests/CreateInvitationRequest.kt
+++ api/src/commonMain/kotlin/app/meetacy/sdk/engine/requests/CreateInvitationRequest.kt
@@ -2,6 +2,7 @@ package app.meetacy.sdk.engine.requests
 
 import app.meetacy.sdk.types.auth.Token
 import app.meetacy.sdk.types.datetime.DateTime
+import app.meetacy.sdk.types.invitation.Invitation
 import app.meetacy.sdk.types.meeting.MeetingId
 import app.meetacy.sdk.types.user.User
 
@@ -10,4 +11,6 @@ public data class CreateInvitationRequest(
     val invitedUser: User,
     val expiryDate: DateTime,
     val meeting: MeetingId
-) : SimpleMeetacyRequest
+) : MeetacyRequest<CreateInvitationRequest.Response> {
+    public data class Response(val invitation: Invitation)
+}
