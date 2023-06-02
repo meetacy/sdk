5a4901fa067b285e839056ef17fad8df35fa3ccc
bpavuk <boopentiy@gmail.com>
2023-06-02 19:54:07 +0300

    Updated Invitations API

--- api/src/commonMain/kotlin/app/meetacy/sdk/invitations/InvitationsApi.kt
+++ api/src/commonMain/kotlin/app/meetacy/sdk/invitations/InvitationsApi.kt
@@ -4,12 +4,25 @@ import app.meetacy.sdk.MeetacyApi
 import app.meetacy.sdk.engine.requests.CreateInvitationRequest
 import app.meetacy.sdk.types.auth.Token
 import app.meetacy.sdk.types.datetime.DateTime
-import app.meetacy.sdk.types.invitation.Invitation
 import app.meetacy.sdk.types.meeting.MeetingId
 import app.meetacy.sdk.types.user.User
 
 public class InvitationsApi(private val api: MeetacyApi) {
-    public suspend fun create(token: Token, invitedUser: User, expiryDate: DateTime, meetingId: MeetingId): Invitation {
-        return api.engine.execute(CreateInvitationRequest(token, invitedUser, expiryDate, meetingId)).invitation
+    public suspend fun create(
+        token: Token,
+        invitedUser: User,
+        expiryDate: DateTime,
+        meetingId: MeetingId
+    ): InvitationsRepository {
+        val invitation = api.engine.execute(
+            request = CreateInvitationRequest(
+                token,
+                invitedUser,
+                expiryDate,
+                meetingId
+            )
+        ).invitation
+
+        return InvitationsRepository(invitation)
     }
 }
