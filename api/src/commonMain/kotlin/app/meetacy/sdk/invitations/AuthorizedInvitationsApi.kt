906a2f5d835dd93f8f710d3314c894895b28012e
bpavuk <boopentiy@gmail.com>
2023-06-02 19:55:03 +0300

    Updated AuthorizedInvitationsApi

--- api/src/commonMain/kotlin/app/meetacy/sdk/invitations/AuthorizedInvitationsApi.kt
+++ api/src/commonMain/kotlin/app/meetacy/sdk/invitations/AuthorizedInvitationsApi.kt
@@ -3,7 +3,6 @@ package app.meetacy.sdk.invitations
 import app.meetacy.sdk.AuthorizedMeetacyApi
 import app.meetacy.sdk.types.auth.Token
 import app.meetacy.sdk.types.datetime.DateTime
-import app.meetacy.sdk.types.invitation.Invitation
 import app.meetacy.sdk.types.meeting.MeetingId
 import app.meetacy.sdk.types.user.User
 
@@ -11,6 +10,13 @@ public class AuthorizedInvitationsApi(private val api: AuthorizedMeetacyApi) {
     public val token: Token get() = api.token
     public val base: InvitationsApi = api.base.invitations
 
-    public suspend fun create(invitedUser: User, expiryDate: DateTime, meetingId: MeetingId): Invitation =
-        base.create(token, invitedUser, expiryDate, meetingId)
+    public suspend fun create(
+        invitedUser: User,
+        expiryDate: DateTime,
+        meetingId: MeetingId
+    ): AuthorizedInvitationRepository {
+        val invitation = base.create(token, invitedUser, expiryDate, meetingId).data
+
+        return AuthorizedInvitationRepository(invitation)
+    }
 }
