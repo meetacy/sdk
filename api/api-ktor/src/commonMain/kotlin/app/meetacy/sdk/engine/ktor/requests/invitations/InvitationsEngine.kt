aeded94dd943e2411bc7b3c64c1fb06d52b6cba2
bpavuk <boopentiy@gmail.com>
2023-06-03 12:56:45 +0300

    Added new function to engine

--- api/api-ktor/src/commonMain/kotlin/app/meetacy/sdk/engine/ktor/requests/invitations/InvitationsEngine.kt
+++ api/api-ktor/src/commonMain/kotlin/app/meetacy/sdk/engine/ktor/requests/invitations/InvitationsEngine.kt
@@ -1,12 +1,14 @@
 package app.meetacy.sdk.engine.ktor.requests.invitations
 
 import app.meetacy.sdk.engine.ktor.toInvitation
+import app.meetacy.sdk.engine.requests.AcceptInvitationRequest
 import app.meetacy.sdk.engine.requests.CreateInvitationRequest
 import app.meetacy.sdk.types.url.Url
 import dev.icerock.moko.network.generated.apis.InvitationsApi
 import dev.icerock.moko.network.generated.apis.InvitationsApiImpl
 import io.ktor.client.*
 import kotlinx.serialization.json.Json
+import dev.icerock.moko.network.generated.models.AcceptInvitationRequest as GeneratedAcceptInvitationRequest
 import dev.icerock.moko.network.generated.models.CreateInvitationRequest as GeneratedCreateInvitationRequest
 
 internal class InvitationsEngine(
@@ -30,5 +32,17 @@ internal class InvitationsEngine(
         ).result
 
         return CreateInvitationRequest.Response(response.toInvitation())
+    }
+
+    suspend fun accept(
+        request: AcceptInvitationRequest
+    ) {
+        base.invitationsAcceptPost(
+            apiVersion = request.apiVersion.int.toString(),
+            acceptInvitationRequest = GeneratedAcceptInvitationRequest(
+                token = request.token.string,
+                invitationIdentity = request.invitationId.string
+            )
+        )
     }
 }
