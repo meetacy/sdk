01e7e73181b166d1007a17f014c0fc3978cc53c3
bpavuk <boopentiy@gmail.com>
2023-06-03 12:58:04 +0300

    Updated invitations repository

--- api/src/commonMain/kotlin/app/meetacy/sdk/invitations/InvitationsRepository.kt
+++ api/src/commonMain/kotlin/app/meetacy/sdk/invitations/InvitationsRepository.kt
@@ -1,5 +1,7 @@
 package app.meetacy.sdk.invitations
 
+import app.meetacy.sdk.MeetacyApi
+import app.meetacy.sdk.types.auth.Token
 import app.meetacy.sdk.types.datetime.DateTime
 import app.meetacy.sdk.types.invitation.Invitation
 import app.meetacy.sdk.types.invitation.InvitationId
@@ -7,11 +9,16 @@ import app.meetacy.sdk.types.meeting.Meeting
 import app.meetacy.sdk.types.user.User
 
 public class InvitationsRepository(
-    public val data: Invitation
+    public val data: Invitation,
+    private val api: MeetacyApi
 ) {
     public val id: InvitationId get() = data.id
     public val expiryDate: DateTime get() = data.expiryDate
     public val invitedUser: User get() = data.invitedUser
     public val invitorUser: User get() = data.invitorUser
     public val meeting: Meeting get() = data.meeting
+
+    public suspend fun accept(token: Token) {
+        api.invitations.accept(token, id)
+    }
 }
