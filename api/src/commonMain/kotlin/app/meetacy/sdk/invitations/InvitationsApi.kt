a916263294b78180254be2e1d310b8b1c03680d2
bpavuk <boopentiy@gmail.com>
2023-06-03 15:42:01 +0300

    Added new functions for invitation reading to the Invitations API

--- api/src/commonMain/kotlin/app/meetacy/sdk/invitations/InvitationsApi.kt
+++ api/src/commonMain/kotlin/app/meetacy/sdk/invitations/InvitationsApi.kt
@@ -3,11 +3,13 @@ package app.meetacy.sdk.invitations
 import app.meetacy.sdk.MeetacyApi
 import app.meetacy.sdk.engine.requests.AcceptInvitationRequest
 import app.meetacy.sdk.engine.requests.CreateInvitationRequest
+import app.meetacy.sdk.engine.requests.ReadInvitationRequest
 import app.meetacy.sdk.types.auth.Token
 import app.meetacy.sdk.types.datetime.DateTime
 import app.meetacy.sdk.types.invitation.InvitationId
 import app.meetacy.sdk.types.meeting.MeetingId
 import app.meetacy.sdk.types.user.User
+import app.meetacy.sdk.types.user.UserId
 
 public class InvitationsApi(private val api: MeetacyApi) {
     public suspend fun create(
@@ -37,5 +39,39 @@ public class InvitationsApi(private val api: MeetacyApi) {
                 token, invitationId
             )
         )
+    }
+
+    public suspend fun read(
+        token: Token
+    ): List<InvitationsRepository> {
+        val invitations = api.engine.execute(
+            request = ReadInvitationRequest(
+                token
+            )
+        ).result
+
+        return invitations.map { InvitationsRepository(it, api) }
+    }
+
+    public suspend fun read(
+        token: Token,
+        ids: List<InvitationId>
+    ): List<InvitationsRepository> {
+        val invitations = api.engine.execute(
+            request = ReadInvitationRequest(token, ids = ids)
+        ).result
+
+        return invitations.map { InvitationsRepository(it, api) }
+    }
+
+    public suspend fun readFrom(
+        token: Token,
+        from: List<UserId>
+    ): List<InvitationsRepository> {
+        val invitations = api.engine.execute(
+            request = ReadInvitationRequest(token, from)
+        ).result
+
+        return invitations.map { InvitationsRepository(it, api) }
     }
 }
