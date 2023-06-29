573596ced8b4b736ef2b0e673003404b78d3a457
Alex Sokol / y9san9 <y9san9@gmail.com>
2023-06-29 14:13:02 +0300

    feat: snapshot publication added to CI

--- build-logic/src/main/kotlin/publication-convention.gradle.kts
+++ build-logic/src/main/kotlin/publication-convention.gradle.kts
@@ -15,4 +15,24 @@ publishing {
             }
         }
     }
+
+    publications.withType<MavenPublication> {
+        val snapshot = project.findProperty("snapshot")?.toString()?.toBooleanStrict()
+        if (snapshot == null || !snapshot) return@withType
+
+        val commit = project.property("commit").toString()
+        val attempt = project.property("attempt").toString().toInt()
+
+        afterEvaluate {
+            val version = buildString {
+                append(this@withType.version)
+                append("-build")
+                append(commit.take(n = 7))
+                if (attempt > 1) {
+                    append(attempt)
+                }
+            }
+            println(version)
+        }
+    }
 }
