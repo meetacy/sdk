e47c33533fb6c8f172f9179e3ea492941000bcbc
bpavuk <boopentiy@gmail.com>
2023-06-30 16:50:14 +0300

    added username validation to auth api

--- api/src/commonMain/kotlin/app/meetacy/sdk/auth/AuthApi.kt
+++ api/src/commonMain/kotlin/app/meetacy/sdk/auth/AuthApi.kt
@@ -5,15 +5,20 @@ import app.meetacy.sdk.MeetacyApi
 import app.meetacy.sdk.auth.email.EmailApi
 import app.meetacy.sdk.engine.requests.GenerateAuthRequest
 import app.meetacy.sdk.types.auth.Token
+import app.meetacy.sdk.types.user.Username
 
 public class AuthApi(private val api: MeetacyApi) {
     public val email: EmailApi = EmailApi(api)
 
     public suspend fun generate(nickname: String): Token {
+        validateUsername(nickname)
         return api.engine.execute(GenerateAuthRequest(nickname)).token
     }
 
     public suspend fun generateAuthorizedApi(nickname: String): AuthorizedMeetacyApi {
         return api.authorized(generate(nickname))
     }
+
+    public suspend fun validateUsername(username: String): Username =
+        api.users.validateUsername(username)
 }
