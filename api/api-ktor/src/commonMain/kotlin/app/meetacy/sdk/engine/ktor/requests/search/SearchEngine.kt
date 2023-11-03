805efc99f0df7281ff104794ec54f69ec08dde89
y9Kap <wotblitz6allima4ok@gmail.com>
2023-11-04 00:59:19 +0300

    fix(#79-support-search): setBody remove at get search request

--- api/api-ktor/src/commonMain/kotlin/app/meetacy/sdk/engine/ktor/requests/search/SearchEngine.kt
+++ api/api-ktor/src/commonMain/kotlin/app/meetacy/sdk/engine/ktor/requests/search/SearchEngine.kt
@@ -4,14 +4,11 @@ import app.meetacy.sdk.engine.ktor.apiVersion
 import app.meetacy.sdk.engine.ktor.response.bodyAsSuccess
 import app.meetacy.sdk.engine.ktor.token
 import app.meetacy.sdk.engine.requests.SearchRequest
-import app.meetacy.sdk.types.serializable.location.LocationSerializable
-import app.meetacy.sdk.types.serializable.location.serializable
 import app.meetacy.sdk.types.serializable.search.SearchItemSerializable
 import app.meetacy.sdk.types.serializable.search.type
 import app.meetacy.sdk.types.url.Url
 import io.ktor.client.*
 import io.ktor.client.request.*
-import kotlinx.serialization.Serializable
 
 internal class SearchEngine(
     baseUrl: Url,
@@ -19,16 +16,13 @@ internal class SearchEngine(
 ) {
     val baseUrl: Url = baseUrl / "search"
 
-    @Serializable
-    private data class SearchBody(val location: LocationSerializable, val promt: String)
-    private fun SearchRequest.toBody() = SearchBody(location.serializable(), prompt)
-
     suspend fun search(request: SearchRequest): SearchRequest.Response {
-        val body = request.toBody()
         val response =  httpClient.get(baseUrl.string) {
             apiVersion(request.apiVersion)
             token(request.token)
-            setBody(body)
+            parameter("latitude", request.location.latitude)
+            parameter("longitude", request.location.longitude)
+            parameter("prompt", request.prompt)
         }.bodyAsSuccess<List<SearchItemSerializable>>()
         return SearchRequest.Response(response.map { it.type() })
     }
