58487fa79d5196e10f85a731ab45e0be695159b8
y9Kap <wotblitz6allima4ok@gmail.com>
2024-03-07 23:59:32 +0300

    fix(#93-add-getSubscriptions-and-getSubscribers-methods): promt in first place in search request

--- api/src/commonMain/kotlin/app/meetacy/sdk/engine/requests/SearchRequest.kt
+++ api/src/commonMain/kotlin/app/meetacy/sdk/engine/requests/SearchRequest.kt
@@ -5,9 +5,9 @@ import app.meetacy.sdk.types.location.Location
 import app.meetacy.sdk.types.search.SearchItem
 
 public data class SearchRequest(
+    val prompt: String,
     val token: Token,
-    val location: Location?,
-    val prompt: String
+    val location: Location?
 ) : MeetacyRequest<SearchRequest.Response> {
     public data class Response(val items: List<SearchItem>)
 }
